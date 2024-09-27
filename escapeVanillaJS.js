034efba13554969018b9868169591232723599c0
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-09-27 14:00:45 +0200

    add comments to clarify code, await the new promise in the async function navigateLabyrinth

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function findMostRecentBook(books) {
-    // 🪲 Bug: Logic error
+    // change comparison operator, we are looking for recent not old books 
     return books.reduce((mostRecent, book) => new Date(book.published) > new Date(mostRecent.published) ? book : mostRecent);
 }
 
@@ -47,8 +47,8 @@ function findMostRecentBook(books) {
 
 async function navigateLabyrinth(directions) {
     for (let direction of directions) {
-        // 🪲 Bug: No delay
-        new Promise(resolve => setTimeout(resolve, 1000));
+       //should await things that return promises- async function always returns a promise  
+        await new Promise(resolve => setTimeout(resolve, 1000));
         console.log(`Navigating: ${direction.step}`);
     }
     return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
