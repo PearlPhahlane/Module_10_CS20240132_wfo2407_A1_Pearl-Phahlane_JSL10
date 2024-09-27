634b81e1596d74d7e2788a24a170ba09bc8eb41a
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-09-27 14:26:23 +0200

    removed comments to make my code more readable

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(response => response.json())
             .then(books => {
                 const mostRecentBook = findMostRecentBook(books);
-                // 🪲 Bug: Incorrect element ID
                 document.getElementById("room1Result").textContent = `The key to the next room is: ${mostRecentBook.title}`;
             });
     });
@@ -16,9 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     document.getElementById("solveRoom2").addEventListener("click", () => {
         const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'async']);
-        // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
-        // 🪲 Bug: Incorrect function call
         const commonConcepts = findIntersection(jsConcepts, reactConcepts);
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
